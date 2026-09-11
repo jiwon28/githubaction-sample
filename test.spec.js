ed2605f23555c54bee0db3f118c4129a1841a943
jiwon28 <sjw_3102@naver.com>
2026-09-11 12:42:35 +0900

    git hub action sample

--- test.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Default Test Set', () => {
-  it('test1 should be passed', () => {
-    console.log('test1 passed');
-  });
-
-  it('test2 should be passed', () => {
-    console.log('test2 passed');
-  });
-});
